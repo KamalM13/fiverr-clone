280a2a90b35ef2a3e7a014855d8d073513123bcb
Kamal Mohamed <kamal.mohamed@msa.edu.eg>
2024-03-04 11:06:59 +0200

    Added navbar for gigs page, added hover for fiverr pro

--- fiverr-clone-view/tailwind.config.js
+++ fiverr-clone-view/tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         main: "#404145",
+        main2: "#74767e",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
