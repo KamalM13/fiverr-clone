eac4ea8cb5f0352b2563dc53408e5ffba2f142c3
Kamal Mohamed <kamal.mohamed@msa.edu.eg>
2024-05-01 20:47:57 +0300

    Fixed many console errors

--- fiverr-clone-view/src/components/navbar/Extra-navbar.tsx
+++ fiverr-clone-view/src/components/navbar/Extra-navbar.tsx
@@ -4,7 +4,7 @@ import {
     HoverCardContent,
     HoverCardTrigger,
 } from "@/components/ui/hover-card"
-
+import { v4 as uuidv4 } from 'uuid';
 
 
 
@@ -21,32 +21,28 @@ const Extranavbar = () => {
             <hr className="hidden md:inline w-full border-1 " />
             <div className="hidden md:flex w-full px-4 md:px-7 justify-between text-gray-500 pt-2">
                 {objects.map((object, idx) => (
-                    <HoverCard>
+                    <HoverCard key={idx}>
                         <HoverCardTrigger className="cursor-pointer hover:border-b-4 border-green-500 pb-2 hover:pb-1">{object}</HoverCardTrigger>
-                        <HoverCardContent >
-                            <div className="">
+                        <HoverCardContent>
+                            <div className="" key={idx}>
                                 <div className="bg-white w-screen grid grid-cols-4 justify-between items-center gap-x-10 p-3">
                                     {Array.from({ length: 4 }).map((_, index) => (
-                                        <>
-                                            <div className="flex flex-col gap-y-2 p-2 text-lg" key={index}>
-                                                <p className="font-bold">Logo & Brand Identity</p>
-                                                {Array.from({ length: 5 }).map((_, index) => (
-                                                    <Link to={'/gigs?category='+categories[idx]} key={index} className="hover:text-gray-400"> Logo Design</Link>
-                                                ))}
-                                            </div>
-                                        </>
+                                        <div key={uuidv4()} className="flex flex-col gap-y-2 p-2 text-lg">
+                                            <p className="font-bold">Logo & Brand Identity</p>
+                                            {Array.from({ length: 5 }).map((_, innerIndex) => (
+                                                <Link to={'/gigs?category=' + categories[idx]} key={uuidv4()} className="hover:text-gray-400"> Logo Design</Link>
+                                            ))}
+                                        </div>
                                     ))}
                                 </div>
                                 <div className="bg-white w-screen grid grid-cols-4 justify-between items-center gap-x-10 p-3">
                                     {Array.from({ length: 4 }).map((_, index) => (
-                                        <>
-                                            <div className="flex flex-col gap-y-2 p-2 text-lg" key={index}>
-                                                <p className="font-bold">Logo & Brand Identity</p>
-                                                {Array.from({ length: 5 }).map((_, index) => (
-                                                    <Link to={'/gigs?category='+categories[idx]} key={index} className="hover:text-gray-400"> Logo Design</Link>
-                                                ))}
-                                            </div>
-                                        </>
+                                        <div key={uuidv4()} className="flex flex-col gap-y-2 p-2 text-lg">
+                                            <p className="font-bold">Logo & Brand Identity</p>
+                                            {Array.from({ length: 5 }).map((_, innerIndex) => (
+                                                <Link to={'/gigs?category=' + categories[idx]} key={uuidv4()} className="hover:text-gray-400"> Logo Design</Link>
+                                            ))}
+                                        </div>
                                     ))}
                                 </div>
                             </div>
